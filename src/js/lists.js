54d86d127da8845d3f121f9afc4152766587842b
Tam-Weibel <tamaraweibel.comgit config --global user.email tamaraweibel@gmail.com>
2024-01-19 11:22:30 +0100

    favorites list created

--- src/js/lists.js
+++ src/js/lists.js
@@ -5,31 +5,58 @@ const searchBtn = document.querySelector(".js-btn");
 const resultsList = document.querySelector(".js-results-list");
 const favoritesList = document.querySelector(".js-favorites-list");
 let searchResults = [];
-let anime = { title: "", img: "" };
 let results = [];
-let favorites =[];
+let favorites = [];
 
-function renderResults() {
+
+function renderResults(anime) {
+  const addItem = document.createElement("li");
+  const addImg = document.createElement("img");
+  const addText = document.createElement("h4");
+  const addTitle = document.createTextNode(anime.title);
+  addImg.src = anime.img;
+  addImg.alt = "No image available for this series";
+  addText.appendChild(addTitle);
+  addItem.appendChild(addText);
+  addItem.appendChild(addImg);
+  addItem.setAttribute("class", "results__li");
+  addImg.setAttribute("class", "results__img");
+  resultsList.appendChild(addItem);
+  addItem.addEventListener("click", ()=>{
+    favorites.title = anime.title;
+    favorites.img = anime.img;
+    favorites.push(anime);
+    renderFavorites(favorites);
+    
+});
+}
+
+function renderFavorites() {
     const addItem = document.createElement("li");
     const addImg = document.createElement("img");
     const addText = document.createElement("h4");
-    const addTitle = document.createTextNode(anime.title);
-    addImg.src = anime.img;
-    addImg.alt = 'No image available for this series';
+    const addTitle = document.createTextNode(favorites.title);
+    addImg.src = favorites.img;
+    addImg.alt = "No image available for this series";
     addText.appendChild(addTitle);
     addItem.appendChild(addText);
     addItem.appendChild(addImg);
-    addItem.setAttribute('class', 'li');
-    addImg.setAttribute('class', 'img');
-    resultsList.appendChild(addItem);
+    addItem.setAttribute("class", "favorites__li");
+    addImg.setAttribute("class", "favorites__img");
+    favoritesList.appendChild(addItem);
 }
+
 
 function getList() {
   for (let i = 0; i < searchResults.length; i++) {
+    const anime = { title: "", img: "" };
     anime.title = searchResults[i].titles[1].title;
     anime.img = searchResults[i].images.webp.image_url;
-    if(anime.img === 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'){
-        anime.img = '../images/No-Image.svg'
+    if (
+      anime.img ===
+      "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
+    ) {
+      anime.img = "../images/No-Image.svg";
     }
     results.push(anime);
     renderResults(anime);
@@ -37,7 +64,7 @@ function getList() {
 }
 
 function handleSearch(event) {
-  event.preventDefault;
+  event.preventDefault();
   let inputValue = userInput.value.toLowerCase();
   fetch(`https://api.jikan.moe/v4/anime?q=${inputValue}`)
     .then(function (response) {
@@ -45,10 +72,19 @@ function handleSearch(event) {
     })
     .then(function (data) {
       searchResults = data.data;
-      console.log(searchResults);
       getList(searchResults);
       renderResults(searchResults);
+    })
+    .catch(function (error) {
+        console.error("Fetch error:", error);
     });
 }
-
 searchBtn.addEventListener("click", handleSearch);
+
+// let listItem = document.querySelector(".results__li");
+
+// console.log(listItem);
+
+
+
+
